0f15f607005f3bd460c4446d96e756c1768e862a
ChandanSatyendraPrasad <chandansatyendraprasad@gmail.com>
2021-10-21 11:56:50 +0530

    Initial commit

---
+++ app/src/main/java/chandan/prasad/androidserviceexample_message/MainActivity.java
@@ -0,0 +1,20 @@
+package chandan.prasad.androidserviceexample_message;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+//Nothing to see here, go to the MainFragment.
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+			.add(R.id.container, new MainFragment()).commit();
+		}
+	}
+
+}
